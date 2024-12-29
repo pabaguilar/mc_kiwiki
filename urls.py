c1fc7727821a5c08828304d29a97b1e4ace17262
pabaguilar <124668660+pabaguilar@users.noreply.github.com>
2024-12-29 20:41:38 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 config = {
-    "wiki_url": "https://microserviciowiki-production.up.railway.app/",
-    "entry_url": "https://microserviceswikientry-production.up.railway.app/",
-    "commentary_url": "https://kiwikibackcommentaries-production.up.railway.app/",
+    "wiki_url": "https://microserviciowiki-production.up.railway.app/wikis",
+    "entry_url": "https://microserviceswikientry-production.up.railway.app/entries",
+    "commentary_url": "https://kiwikibackcommentaries-production.up.railway.app/commentaries",
     "notification_url": "http://notifications:8004/notification",
-    "version_url": "https://microservicioversion-production.up.railway.app/",
-    "user_url": "https://kiwikibackuser-production.up.railway.app/"
+    "version_url": "https://microservicioversion-production.up.railway.app/versions",
+    "user_url": "https://kiwikibackuser-production.up.railway.app/users"
 }
